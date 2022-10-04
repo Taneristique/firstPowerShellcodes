28d654a58b3339d8f5a0526b015a444cd6b2fef8
Taneristique <grkmtnrackz@outlook.fr>
2022-10-04 10:01:39 +0300

    Calculate factorielle of any number

---
+++ factorielle.ps1
@@ -0,0 +1,16 @@
+﻿$decide="yes"
+do{
+    $number=Read-host -prompt "Please provide an integer to calculate its factorielle" 
+    function factorielle{   
+        Param([int] $n)
+        $l=$n
+        while($l -gt 1){
+            $n=($l-1)*$n 
+            $l-=1
+        }
+        Write-Host $n 
+    }
+    factorielle($number)
+    $decide=Read-Host -Prompt "would you want to continue?(yes/no)"
+    }
+ while($decide -eq "yes")
